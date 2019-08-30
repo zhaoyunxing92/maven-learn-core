259546baf37ab6e54aab70d859e00032e4ddcb76
zhaoyunxing92 <2385585770@qq.com>
2019-08-30 10:56:46 +0800

    [add]使用fastjson处理返回

--- src/main/java/io/github/sunny/core/adapter/WebMvcConfigurerAdapter.java
+++ src/main/java/io/github/sunny/core/adapter/WebMvcConfigurerAdapter.java
@@ -3,12 +3,23 @@
  */
 package io.github.sunny.core.adapter;
 
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import io.github.sunny.bean.WebBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zhaoyunxing
@@ -22,5 +33,38 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfigurerAdapter implements WebMvcConfigurer {
     public WebMvcConfigurerAdapter() {
         log.error("WebMvcConfigurerAdapter bean loading....");
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 删除MappingJackson2HttpMessageConverter
+        converters.removeIf(httpMessageConverter -> httpMessageConverter instanceof MappingJackson2HttpMessageConverter);
+
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        SerializerFeature[] serializerFeatures = new SerializerFeature[]{
+                SerializerFeature.WriteMapNullValue,
+                // todo: 解决 BigDecimal整数补位问题注释该代码，否则null的值都转为0
+                // SerializerFeature.WriteNullNumberAsZero,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullBooleanAsFalse,
+                SerializerFeature.WriteDateUseDateFormat,
+                SerializerFeature.DisableCircularReferenceDetect,
+        };
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+
+        // serializeConfig.put(BigDecimal.class, BigDecimalFormat.instance);
+
+        fastJsonConfig.setSerializeConfig(serializeConfig);
+        fastJsonConfig.setSerializerFeatures(serializerFeatures);
+        fastJsonConfig.setCharset(Charset.forName("UTF-8"));
+        //fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        List<MediaType> mediaTypes = new ArrayList<>();
+        //设定json格式且编码为UTF-8
+        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(mediaTypes);
+        converters.add(fastConverter);
     }
 }
