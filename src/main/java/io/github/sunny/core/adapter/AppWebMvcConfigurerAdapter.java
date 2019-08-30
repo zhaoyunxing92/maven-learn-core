ba2eefa171a813ca7e07ac1b498ad0e2e02e9387
zhaoyunxing92 <2385585770@qq.com>
2019-08-30 10:41:05 +0800

    [up]删除冗余包

--- src/main/java/io/github/sunny/core/adapter/AppWebMvcConfigurerAdapter.java
+++ src/main/java/io/github/sunny/core/adapter/AppWebMvcConfigurerAdapter.java
@@ -5,8 +5,6 @@ package io.github.sunny.core.adapter;
 
 import io.github.sunny.bean.AppBean;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
